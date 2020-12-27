73002ab71eb955b08d69c3f0008d018934d2b475
Timorsa <timor.saf@gmail.com>
2020-12-27 19:45:54 +0200

    added passport support

--- server.js
+++ server.js
@@ -19,15 +19,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(passport.initialize());
+app.use(passport.initialize());
 
 // passport config 
-// require('./src/config/passport')(passport)
+require('./src/config/passport')(passport)
 
-//* use Routes
+
 app.use("/api/user", userRoute);
-
-// error handler
 app.use(errorHandler);
 
 
