046232c8e1f78e89630b9324a5e7266b37c3b3df
Timorsa <timor.saf@gmail.com>
2020-12-31 11:09:59 +0200

    check

--- src/config/keys.js
+++ src/config/keys.js
@@ -3,7 +3,7 @@
  ***/
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./keys_prod');
+    module.exports = require('./keys_dev');
 } else {
     module.exports = require('./keys_dev');
 }
